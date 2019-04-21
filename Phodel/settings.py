8e29fe312f14869f3ad4c432836068bb7fdb5c92
Adams Temidire <gr8temi@gmail.com>
2019-04-21 14:33:23 +0100

    Database Change

--- Phodel/settings.py
+++ Phodel/settings.py
@@ -127,8 +127,13 @@ STAR_RATINGS_STAR_HEIGHT=12
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'phodel',
+        'USER': 'gr8temi',
+        'PASSWORD': 'ABRAHAM13/0609',
+        'HOST': 'gr8temi.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+
     }
 }
 
